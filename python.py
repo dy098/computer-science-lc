2ee358c85ff018cb4b255e0d0588cb411b93d2e0
dy098 <90386643+dy098@users.noreply.github.com>
2021-09-09 10:55:11 +0100

    Add files via upload

---
+++ python.py
@@ -0,0 +1,17 @@
+#progeamme to show maths operators
+number1 =5
+number2 =7
+#additon 
+print(number1+number2)
+#multiplacation
+print(number1*number2)
+# exponent number1 to the power of number2
+print(number1 ** number2)
+#divison
+print(number1/number2)
+#floor devide lowest whole number after divison
+print(number1 // number2)
+#modulas remainder in whole numbers after devision
+print(number1 % number2)
+#subtraction
+print(number1-number2)
