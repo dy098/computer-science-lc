86a8e0898a1ec61c128c264b240765e5ea349374
dylan <90386643+dy098@users.noreply.github.com>
2021-09-20 12:34:04 +0100

    Add files via upload

---
+++ chapter 1/python 2.py
@@ -0,0 +1,24 @@
+# task 1
+# number1 =16
+# print("what a great age",number1)
+# #task 2
+# number1= 2.4
+# number2= 3.6
+# print(number1,number2)
+# task 3
+# division=number1+number2/2
+# print(division)
+# remainder=number1%number2
+# print(remainder)
+# powerOf=number1**number2
+# print(powerOf)
+# fahrenheit=int(input("enter trmp: "))
+# celsius=(5/9)* (fahrenheit-32)
+# print(round(celsius,2),"c")
+# #task5
+miles=7100
+km=miles*1.60935
+print(km)
+cost=900
+flight=cost*km
+print(flight)
