0f3bf0296835229d12b1e6a21aaabc3792e6f4f3
dylan <90386643+dy098@users.noreply.github.com>
2021-09-23 10:56:30 +0100

    Add files via upload

---
+++ chapter 1/pg 25 task 4.py
@@ -0,0 +1,2 @@
+userSecs=int(input("enter a 5 digit number: "))
+print(userSecs/60,"mins and Secs")
---
+++ chapter 1/pg21.py
@@ -0,0 +1,17 @@
+string1= ""
+string1=string1.upper()
+print(string1)
+
+string1= ""
+string1=string1.lower()
+print(string1)
+
+#capitalize in american spelling 
+string1= ""
+string1=string1.capitalize()
+print(string1)
+
+myString="frank lampey"
+print(myString[11])
+print(myString[-1])
+print(myString[5:])
