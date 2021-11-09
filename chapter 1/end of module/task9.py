dfe27f104cb48285a0cc3c8978c8977bc4d41e79
dylan <90386643+dy098@users.noreply.github.com>
2021-11-09 17:31:32 +0000

    Create task9.py

---
+++ chapter 1/end of module/task9.py
@@ -0,0 +1,9 @@
+olympicvalue=input("name one olympic value: ")
+if olympicvalue=="respect":
+    print("thats correct")
+elif olympicvalue=="excellence":
+    print("thats correct")
+elif olympicvalue== "friendship":
+    print("thats correct")
+else:
+    print("Try again")
