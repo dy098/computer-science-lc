d7af2c7584a36e5e64eb789d7a8de3617ccad0e5
dylan <90386643+dy098@users.noreply.github.com>
2021-11-11 10:53:32 +0000

    Add files via upload

---
+++ chapter 1/challange 12.py
@@ -0,0 +1,19 @@
+def capitalize_each_word(original_str):
+    result = ""
+    # Split the string and get all words in a list
+    list_of_words = original_str.split()
+    # Iterate over all elements in list
+    for elem in list_of_words:
+        # capitalize first letter of each word and add to a string
+        if len(result) > 0:
+            result = result + " " + elem.strip().capitalize()
+        else:
+            result = elem.capitalize()
+    # If result is still empty then return original string else returned capitalized.
+    if not result:
+        return original_str
+    else:
+        return result
+sample_text = "33a. it's GONE too far"
+result = capitalize_each_word(sample_text)
+print(result)
